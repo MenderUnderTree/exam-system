185e8119266f11c9d3f6de2cc1b15a13ee1755e8
luoweiyao <aircondi001a@gmail.com>
2018-07-28 23:52:21 +0800

    * init PaperController

---
+++ exam-system-backend/src/main/java/com/thoughtworks/examsystem/controller/PaperController.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.examsystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author : luoweiyao
+ * @created : 2018/7/28
+ * @project : exam-system
+ * @package : com.thoughtworks.examsystem.controller
+ */
+@RestController
+@RequestMapping("/exam")
+public class PaperController {
+
+}
