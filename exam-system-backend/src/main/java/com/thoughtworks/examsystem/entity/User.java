5cafe4da4064abf037b1496035cfeef265769554
luoweiyao <aircondi001a@gmail.com>
2018-07-28 22:14:15 +0800

    add User table

--- exam-system-backend/src/main/java/com/thoughtworks/examsystem/entity/User.java
+++ exam-system-backend/src/main/java/com/thoughtworks/examsystem/entity/User.java
@@ -14,4 +14,28 @@ public class User {
 
     @Column(length = 25, nullable = false)
     private String pwd;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
 }
