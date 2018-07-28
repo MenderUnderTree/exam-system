31f4cd2a7f6d13bda4ab8dff0f3d6a2a33e8a0db
shadow <yingzhang@thoughtworks.com>
2018-07-28 23:09:20 +0800

    Build the infrastructure

---
+++ front-end/demo/src/components/index.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import PaperList from './PaperList/index';
+import PaperContent from './PaperContent/index';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount () {
+        console.log(1)
+    }
+
+    render() {
+        return (
+            <section>
+                <header className="App-header">
+                    <h1 className="App-title">555 <span>做更好的自己</span></h1>
+                </header>
+                <section className="App-content">
+                    <HashRouter basename="/" >
+                        <Switch>
+                            <Route path="/paperlist" exact component={PaperList}></Route>
+                            <Route path="/papercontent" exact component={PaperContent}></Route>
+                            <Redirect to="/" />
+                        </Switch>
+                    </HashRouter>
+                </section>
+
+            </section>
+
+        );
+    }
+}
+
+export default App;
