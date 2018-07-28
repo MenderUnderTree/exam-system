6dc490ad8c90d8d66bdf347ce3b8ffdc69d3efcd
luoweiyao <aircondi001a@gmail.com>
2018-07-28 22:13:35 +0800

    add User table

--- exam-system-backend/src/main/java/com/thoughtworks/examsystem/entity/User.java
+++ exam-system-backend/src/main/java/com/thoughtworks/examsystem/entity/User.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.examsystem.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "user_name", length = 25, nullable = false, unique = true)
+    private String userName;
+
+    @Column(length = 25, nullable = false)
+    private String pwd;
 }
